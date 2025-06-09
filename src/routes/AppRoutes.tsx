9dcefb93eddb373f781a0c546015c4041b89c792
João Andreassa <joaovra@gmail.com>
2025-06-09 16:19:36 -0300

    add

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,17 +1,16 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { LoginPage } from '../pages/LoginPage';
 import { DashboardPage } from '../pages/DashboardPage';
 import EventsPage from '../pages/EventPage';
 
 export function AppRoutes() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/dashboard" element={<DashboardPage />} />
         <Route path="/events" element={<EventsPage />} />
-
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
