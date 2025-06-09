bf88ae509d93c839e19e52a5b27a43618ae86af5
João Andreassa <joaovra@gmail.com>
2025-06-09 16:05:36 -0300

    add

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/tropa-digital/',
+  base: '/Tropa-Digital/',
   plugins: [react()],
 })
